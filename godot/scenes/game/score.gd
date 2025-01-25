c093e47e9b64a35ea1616744168f375b7f7e04c5
jrhath0 <jr.hatheway0@gmail.com>
2025-01-25 17:50:22 -0600

    Added Score HUD
    Added score display to main game scene

--- godot/scenes/game/score.gd
+++ godot/scenes/game/score.gd
@@ -4,7 +4,7 @@ var total_score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	update_score_label() #Initialize label at start of main game screen
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,5 +16,9 @@ func _on_play_area_popped_bubbles(bubbles: Array[Bubble]) -> void:
 	for bubble in bubbles:
 		total_score += bubble.score
 	print(total_score)
-	pass # Replace with function body.
-	
+	update_score_label() #Update score label after score is calculated
+
+
+func update_score_label():
+	var score_label =  $HUD/Label
+	score_label.text = "Score: " + str(total_score)
